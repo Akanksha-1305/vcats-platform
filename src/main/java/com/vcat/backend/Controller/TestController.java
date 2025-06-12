ae88119739da157451fbb08d91a5ad6e84640e8e
Akanksha <162199184+Akanksha-1305@users.noreply.github.com>
2025-06-12 15:48:48 +0530

    Create TestController.java

---
+++ src/main/java/com/vcat/backend/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.vcats.platform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Backend is working!";
+    }
+}
